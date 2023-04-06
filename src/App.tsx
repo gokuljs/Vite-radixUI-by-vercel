9967afcd4cad8283fc681f7205a04f5ae99360b4
gokuljs <gokuljs029@gmail.com>
2023-04-06 14:27:35 +0530

    testing vite esbundle with vercel

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState<number>(0);
+  const [test, setTest] = useState<boolean>(false);
+
+  return (
+    <div className="App">
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://reactjs.org" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React + {test.toString()}</h1>
+      <div className="card">
+        <button
+          onClick={() => {
+            setCount((count) => count + 1);
+            setTest(!test);
+          }}
+        >
+          count is {count} {test ? "true" : "false"}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </div>
+  );
+}
+
+export default App;
